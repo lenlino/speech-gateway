d9fe795107fdacc4ebed99a4e2ab5e37f44b4a2b
lenlino <65199014+lenlino@users.noreply.github.com>
2025-01-22 14:09:36 +0900

    Fix README encoding

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     maintainer="uezo",
     maintainer_email="uezo@uezo.net",
     description="A reverse proxy server that enhances speech synthesis with essential, extensible features. 🦉💬",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests*"]),
     install_requires=["aiofiles==24.1.0", "fastapi==0.115.6", "httpx==0.28.1", "uvicorn==0.34.0"],
